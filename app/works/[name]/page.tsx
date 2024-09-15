035e51caa70aa7108d24d104765ad0c41b28bd34
Ethan Storm <github.balsamic226@passinbox.com>
2024-09-15 14:13:32 -0700

    fixed undefined issue

--- app/works/[name]/page.tsx
+++ app/works/[name]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useParams } from 'next/navigation';
 import React from 'react';
-import { Container, Button } from '@mantine/core';
+import { Button } from '@mantine/core';
 import Link from 'next/link';
 import quantumHollywood1 from '../../../public/works/quantum-hollywood-1.jpg';
 import { WorkHeader } from '@/components/WorkHeader/WorkHeader';
@@ -21,14 +21,15 @@ export default function page() {
   const params = useParams<{ name: string }>();
 
   const work = works.find((piece) => piece.name === params.name);
+
   return (
-    <Container>
-      <WorkHeader work={work} />
+    <>
+      {work ? <WorkHeader work={work} /> : 'That piece does not exist'}
       <Link href="/inquire">
         <Button variant="outline" color="blue" m="lg">
           Inquire
         </Button>
       </Link>
-    </Container>
+    </>
   );
 }
